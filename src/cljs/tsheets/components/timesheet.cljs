9a3fa31cb17f40dd64dfe6ae72e025054c3b548a
Andrew Chumich <andrewchumich@gmail.com>
2015-12-11 11:28:02 -0700

    working on notes input

---
+++ src/cljs/tsheets/components/timesheet.cljs
@@ -0,0 +1,102 @@
+(ns tsheets.components.timesheet
+  (:require [reagent.core :as reagent :refer [atom]]
+            [cljs.core.async :as async
+             :refer [<! >! chan put! timeout]])
+  (:require-macros [cljs.core.async.macros :refer [go]]))
+
+
+(defn set-notes [ts notes]
+  (swap! ts assoc-in [:notes] notes))
+
+(enable-console-print!)
+(defn select-jobcode [ts jobcode]
+  (if (string? jobcode)
+    (swap! ts assoc-in [:jobcode] jobcode)))
+
+
+(defn valid-clock-in? [ts]
+  (if (string? (:jobcode @ts))
+    true
+    false))
+
+
+(defn is-clocked-in? [ts]
+  (= (type (:start @ts)) js/Date))
+
+(defn valid-clock-out? [ts]
+  (if (and (valid-clock-in? ts) (is-clocked-in? ts))
+    true
+    false
+    ))
+
+(defn clock-in [ts]
+  (if (valid-clock-in? ts) 
+    (swap! ts assoc-in [:start] (new js/Date))
+    ))
+
+(defn clock-out [ts]
+  (if (valid-clock-out? ts)
+    (do
+      (swap! ts assoc-in [:end] (new js/Date))
+      )
+    )
+  )
+
+(defn notes-component [{:keys [notes on-save on-stop on-change]}]
+  (println "NOTES")
+  (let [val (atom notes)
+        stop #(do (reset! val "")
+                  (if on-stop (on-stop)))
+        save #(let [v (-> @val str clojure.string/trim)]
+                (on-save v))]
+    (fn [] [:div {:class "container"}
+            [:input {:type "text"
+                     :value @val
+                     :on-blur save
+                     :on-change #(reset! val (-> % .-target .-value))
+                     :on-key-down #(case (.-which %)
+                                     13 (save)
+                                     27 (stop)
+                                     nil)}]
+            ])))
+
+(defn clock-in-component [{:keys [timesheet clocked-in on-clock-out on-clock-in]}]
+  (if (true? clocked-in)
+    [:div
+     [:input {:type "button"
+              :value "Clock Out"
+              :class "clocked-in"
+              :on-click #(on-clock-out)}]]
+    [:div
+     [:input {:type "button"
+              :class "clocked-out"
+              :value "Clock In"
+              :on-click #(on-clock-in)}]]))
+
+(defn jobcode-component [{:keys [jobcodes timesheet on-select]}]
+  (let [ts @timesheet]
+    [:div {:class "container"}
+     [:p "Jobcodes"]
+     (for [jobcode @jobcodes]
+       [:div {:key jobcode}
+        [:input {:type "button"
+                 :class (str "jobcode-list  " (if (= jobcode (:jobcode ts)) "selected"))
+                 :value jobcode
+                 :on-click #(on-select (-> % .-target .-value))}]]
+       )]
+    ))
+
+(defn timesheet-component [{:keys [timesheet jobcodes on-clock-out]}]
+  (println @timesheet)
+  [:div
+   [jobcode-component {:jobcodes jobcodes
+                       :timesheet timesheet
+                       :on-select #(select-jobcode timesheet %)}]
+   [notes-component {:notes (:notes timesheet)
+                     :on-change #(set-notes timesheet %)
+                     :on-save #(set-notes timesheet %)}]
+   [clock-in-component {:timesheet timesheet
+                        :clocked-in (is-clocked-in? timesheet)
+                        :on-clock-in #(clock-in timesheet)
+                        :on-clock-out #(do ((clock-out timesheet)
+                                            (on-clock-out)))}]])
