92600fdd9849fb620d9ba17f2087a51ac1d4b5f4
Andrew Chumich <andrewchumich@gmail.com>
2015-12-13 18:31:18 -0700

    finished notes

--- src/cljs/tsheets/components/timesheet.cljs
+++ src/cljs/tsheets/components/timesheet.cljs
@@ -43,12 +43,13 @@
   )
 
 (defn notes-component [{:keys [notes on-save on-stop on-change]}]
-  (println notes)
   [:div {:class "container"}
    [:input {:type "text"
             :value notes
-            
             :on-change #(on-change (-> % .-target .-value))
+            :on-key-down #(case (.-which %)
+                            27 (on-change "")
+                            nil)
             }]
    ])
 
